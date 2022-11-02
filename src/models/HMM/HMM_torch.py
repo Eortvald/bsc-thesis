09841f45bc2b22e835c53fc49be467a1df56d97f
Eortvald <eortvald@gmail.com>
2022-11-02 19:08:39 +0100

    hmm class 1.2

--- src/models/HMM/HMM_torch.py
+++ src/models/HMM/HMM_torch.py
@@ -18,22 +18,41 @@ class HiddenMarkovModel(nn.Module):
     """
     Hidden Markov model w. Continues observation density
     """
-    def __init__(self, num_states, observation_dim, emmision_dist):
+    def __init__(self, num_states, observation_dim, emission_dist):
         super(HiddenMarkovModel, self).__init__()
 
         self.N = num_states
-        self.transmission_matrix = nn.Parameter(torch.ones(self.N,self.N)/self.N)
-        self.emission_density = emmision_dist
+        self.transition_matrix = nn.Parameter(torch.ones(self.N,self.N)/self.N)
+        self.emission_density = emission_dist
         self.obs_dim = observation_dim
-        self.state_priors = nn.Parameter(torch.ones(self.K) * 1 / self.K)
-        self.emission_prop = nn.ModuleList([self.emission_density(self.obs_dim) for _ in range(self.N)])
+        self.state_priors = nn.Parameter(torch.ones(self.N) * 1 / self.N)
+        self.emission_models = nn.ModuleList([self.emission_density(self.obs_dim) for _ in range(self.N)])
 
         self.LogSoftMax = nn.LogSoftmax()
 
+    def emission_models_forward(self,X):
+        return torch.stack([state_emission(X) for state_emission in self.emission_models])
+
+    def transution_prop_forward(self,transition):
+
 
     def forward(self,X):
-        A = self.LogSoftMax(self.transmission_matrix)
-        pi_n = self.LogSoftMax(self.state_priors)
+        #init
+        log_A = self.LogSoftMax(self.transmission_matrix)
+        log_pi = self.LogSoftMax(self.state_priors)
+
+        num_subjects = X.shape[0]
+        seq_max = X.shape[1]
+        log_alpha = torch.zeros(num_subjects, seq_max, self.N)
+
+        # time t=0
+        log_alpha[:, 0, :] = log_pi + self.emission_models_forward(X[:, 0])
+
+        # for time:  t = 1 -> seq_max
+        for t in range(1,seq_max):
+            log_alpha[:, t, :] = self.emission_models_forward(X[:, t])
+
+
 
 
 
@@ -46,3 +65,14 @@ class HiddenMarkovModel(nn.Module):
 
         return optimal_seq_Z
 
+
+
+
+if __name__ == '__main__':
+
+    from src.distributions.Watson_torch import Watson
+    HMM = HiddenMarkovModel(num_states=6,observation_dim=3,emission_dist=Watson)
+    for state in HMM.emission_models:
+        print(state.kappa)
+    X = torch.rand(10,3)
+    print(HMM.emission_model_forward(X))
