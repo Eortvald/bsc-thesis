ebc8f409c209e7bda5509be749ac3c78b0396de2
Eortvald <eortvald@gmail.com>
2022-11-08 19:20:46 +0100

    hmm forward working - high likelyhood

--- src/models/HMM/HMM_torch.py
+++ src/models/HMM/HMM_torch.py
@@ -29,6 +29,8 @@ class HiddenMarkovModel(nn.Module):
         self.state_priors = nn.Parameter(torch.ones(self.N) * 1 / self.N)
         self.emission_models = nn.ModuleList([self.emission_density(self.obs_dim) for _ in range(self.N)])
 
+        self.logsoftmax_transition = nn.LogSoftmax(dim=1)
+        self.logsoftmax_prior = nn.LogSoftmax(dim=0)
 
     def emission_models_forward(self, X):
         return torch.stack([state_emission(X) for state_emission in self.emission_models])
@@ -43,9 +45,8 @@ class HiddenMarkovModel(nn.Module):
         """
 
         # init  1)
-        log_A = nn.functional.log_softmax(self.transition_matrix)
-        log_pi = nn.functional.log_softmax(self.state_priors)
-
+        log_A = self.logsoftmax_transition(self.transition_matrix)
+        log_pi = self.logsoftmax_prior(self.state_priors)
         num_subjects = X.shape[0]
         seq_max = X.shape[1]
         log_alpha = torch.zeros(num_subjects, seq_max, self.N)
@@ -53,22 +54,28 @@ class HiddenMarkovModel(nn.Module):
         # time t=0
         # log_pi: (n states priors)
         # emission forward return: -> transpose -> (subject, [state1_prop(x)...stateN_prop(x)])
-        print(self.emission_models_forward(X[:, 0, :]).T)
         log_alpha[:, 0, :] = log_pi + self.emission_models_forward(X[:, 0, :]).T
-        print(log_alpha[:, 0, :])
 
         # Induction 2)
         # for time:  t = 1 -> seq_max
+
         for t in range(1, seq_max):
-            log_alpha[:, t, :] = self.emission_models_forward(X[:, t]) \
+            log_alpha[:, t, :] = self.emission_models_forward(X[:, t, :]).T \
                                  + torch.logsumexp(log_alpha[:, t - 1, :, None] + log_A, dim=1)
 
-        #### Termination 3)
-        print(log_alpha[0, :, :])
-        print(log_alpha[0, :, :].shape)
-        log_prop = torch.logsumexp(log_alpha, dim=2)
+        # Termination 3)
+        # print(log_alpha[:, :, :])
+        print(log_alpha.shape)
+        #print(log_alpha)
 
-        return log_prop  # return log_prop per subject
+        # LogSum over states N for each time t.
+        log_t_sums = torch.logsumexp(log_alpha, dim=2)
+        #print(log_t_sums)
+        # Retrive the alpha for the last time t in the seq.
+
+        log_props = torch.gather(log_t_sums, dim=1, index=torch.tensor([[seq_max-1]] * num_subjects)).squeeze()
+        #faster on GPU than just indexing...according to stackoverflow
+        return log_props.sum()  # return log_prop per subject
 
     def viterbi(self, X):
         # inspiraiton from https://colab.research.google.com/drive/1S__f14spx4BVbtNGIDL27th2NpijZjOl
@@ -80,7 +87,10 @@ class HiddenMarkovModel(nn.Module):
 if __name__ == '__main__':
     from src.distributions.Watson_torch import Watson
 
-    HMM = HiddenMarkovModel(num_states=6, observation_dim=3, emission_dist=Watson)
-    X = torch.rand(2, 8, 3)  # num_subject, seq_max, observation_dim
+    dim = 90
 
-    print(HMM(X))
+    HMM = HiddenMarkovModel(num_states=15, observation_dim=dim, emission_dist=Watson)
+    X = torch.rand(200, 350, dim)  # num_subject, seq_max, observation_dim
+
+    out_prop = HMM(X)
+    print(out_prop)
